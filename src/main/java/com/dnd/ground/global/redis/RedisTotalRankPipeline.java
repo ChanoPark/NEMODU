39717fc59f2f8a55aa093873ea4aaea9ff0e4bb1
ChanoPark <koc081900@korea.ac.kr>
2023-12-20 22:43:14 +0900

    MODIFY: 랭킹 조회 수정
    레디스 파이프라인 수정

--- src/main/java/com/dnd/ground/global/redis/RedisTotalRankPipeline.java
+++ src/main/java/com/dnd/ground/global/redis/RedisTotalRankPipeline.java
@@ -6,6 +6,8 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisCallback;
 
+import java.util.List;
+
 /**
  * @description 회원의 누적 랭킹을 조회하기 위한 Redis Pipeline
  * @author  박찬호
@@ -23,8 +25,13 @@ public class RedisTotalRankPipeline implements RedisCallback<UserRankDto> {
     public UserRankDto doInRedis(RedisConnection connection) throws DataAccessException {
         connection.openPipeline();
 
-        Double score = connection.zSetCommands().zScore(KEY.getBytes(), nickname.getBytes());
-        Long rank = connection.zSetCommands().zRank(KEY.getBytes(), nickname.getBytes());
+        connection.zSetCommands().zScore(KEY.getBytes(), nickname.getBytes());
+        connection.zSetCommands().zRevRank(KEY.getBytes(), nickname.getBytes());
+
+        List<Object> results = connection.closePipeline();
+
+        Double score = (Double) results.get(0);
+        Long rank = (Long) results.get(1);
 
         Integer responseRank = rank == null ? null : rank.intValue();
 
